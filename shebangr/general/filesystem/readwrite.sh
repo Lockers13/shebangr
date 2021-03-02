5d177600323bf8a4de403acdbab3c1d12b502d11
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2021-03-02 20:33:54 +0000

    Added to filesystem utils

--- shebangr/general/filesystem/readwrite.sh
+++ shebangr/general/filesystem/readwrite.sh
@@ -3,7 +3,9 @@ CONTENTS=""
 function get_all() {
     read -p "Filename: " filename 
     CONTENTS=$(cat "$filename")
-    printf '\n%*s\n\n%s\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - "$CONTENTS" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    printf '\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - 
+    printf "%s\n" "$CONTENTS" 
+    printf "\n%*s\n\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 }
 
 function filter_by_line() {
@@ -16,7 +18,9 @@ function filter_by_line() {
     output_file=${output_file:-""}
     CONTENTS=$(sed -n "$num1,$num2 p" "$filename")
     if [[ "$output_file" == "" ]]; then
-        printf '\n%*s\n\n%s\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - "$CONTENTS" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+        printf '\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - 
+        printf "%s\n" "$CONTENTS" 
+        printf "\n%*s\n\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     else
         echo "$CONTENTS" > "$output_file"
         echo "Ok, the specified line range from $filename has been written to $output_file..."
@@ -29,10 +33,40 @@ function filter_by_field() {
     read -p "Field number: " fnum
     read -p "Output file (default => stdout): " output_file
     output_file=${output_file:-""}
-    CONTENTS=$(cat $filename | cut -f$fnum -d$sep)
-    echo "$CONTENTS"
+    CONTENTS=$(cat "$filename" | cut -f"$fnum" -d"$sep")
     if [[ "$output_file" == "" ]]; then
-        printf '\n%*s\n\n%s\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - "$CONTENTS" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+        printf '\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - 
+        printf "%s\n" "$CONTENTS" 
+        printf "\n%*s\n\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    else
+        echo "$CONTENTS" > "$output_file"
+        echo "Ok, the specified line range from $filename has been written to $output_file..."
+    fi
+}
+
+function get_funcs() {
+    declare -a func_names=()
+    read -p "Filename: " filename
+    read -p "Output file (default => stdout): " output_file
+    output_file=${output_file:-""}
+    fbname=$(basename "$filename" | cut -d'.' -f1)
+    ext=$(basename "$filename" | cut -d'.' -f2)
+    case "$ext" in
+    py|rb)
+        keyword="def"
+        ;;
+    *)
+        echo "Error: unrecognized file format => $ext"
+        return
+        ;;
+    esac
+
+    CONTENTS=$(cat "$filename" | grep "$keyword")
+
+    if [[ "$output_file" == "" ]]; then
+        printf '\n%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - 
+        printf "%s\n" "$CONTENTS" 
+        printf "\n%*s\n\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     else
         echo "$CONTENTS" > "$output_file"
         echo "Ok, the specified line range from $filename has been written to $output_file..."
